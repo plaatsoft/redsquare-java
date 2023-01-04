c75f32dbbc0687bd4ed3fa7e264c2ab3eea1cbc1
wplaat <wplaat@gmail.com>
2023-01-04 12:02:58 +0100

    Update build scripts

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_241
 call mvn clean deploy -DskipTests=true
 pause
--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_241
 call java -jar target/redsquare-0.4.1-SNAPSHOT-jar-with-dependencies.jar
 pause
