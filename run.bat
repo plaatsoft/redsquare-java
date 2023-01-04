218c9299b691ef93518033ef30f26ec31f2d244d
wplaat <wplaat@gmail.com>
2023-01-04 12:55:04 +0100

    update start script

--- run.bat
+++ run.bat
@@ -1,3 +1,2 @@
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_241
-call java -jar target/redsquare-0.4.1-SNAPSHOT-jar-with-dependencies.jar
+call "C:\Program Files\Java\jdk1.8.0_241\bin\java" -jar target/redsquare-0.4.1-SNAPSHOT-jar-with-dependencies.jar
 pause
